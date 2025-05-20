3df8c0a782ce7aa66f574bc097f38ec84d38d7a7
zlhshn <zsahin4401@gmail.com>
2025-05-20 19:18:16 +0300

    start project

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "#d6c6ff",
+          200: "#a8b5db",
+          300: "#9ca4ab",
+        },
+        dark: {
+          100: "#221f3d",
+          200: "#0f0d23",
+        },
+        accent: "#ab8bff",
+      },
+    },
+  },
+  plugins: [],
+};
