9dcb31f65fa1034f85bf2458932fa0c26f1a5d48
zlhshn <zsahin4401@gmail.com>
2025-05-23 20:01:29 +0300

    fix:bug

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -20,48 +20,61 @@ const MovieInfo = ({ label, value }: MovieInfoProps) => (
 const MovieDetails = () => {
   const router = useRouter();
   const { id } = useLocalSearchParams();
+
   const { data: movie, loading } = useFetch(() =>
     fetchMovieDetails(id as string)
   );
 
+console.log("Fetching movie with ID:", id);
+
   return (
     <View className="bg-primary flex-1">
-      <ScrollView
-        contentContainerStyle={{
-          paddingBottom: 80,
-        }}
-      >
+      <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
         <View>
           <Image
             source={{
-              uri: `https:// image.tmdb.org/t/p/w500${movie?.poster_path}`,
+              uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
             }}
             className="w-full h-[550px]"
-            resizeMode="stretch"
+            resizeMode="cover"
           />
+
+          <TouchableOpacity className="absolute bottom-5 right-5 rounded-full size-14 bg-white flex items-center justify-center">
+            <Image
+              source={icons.play}
+              className="w-6 h-7 ml-1"
+              resizeMode="stretch"
+            />
+          </TouchableOpacity>
         </View>
+
         <View className="flex-col items-start justify-center mt-5 px-5">
           <Text className="text-white font-bold text-xl">{movie?.title}</Text>
           <View className="flex-row items-center gap-x-1 mt-2">
             <Text className="text-light-200 text-sm">
-              {movie?.release_date?.split("-")[0]}
+              {movie?.release_date?.split("-")[0]} •
             </Text>
             <Text className="text-light-200 text-sm">{movie?.runtime}m</Text>
-          </View>{" "}
-          <View className="flex-row items-center bg-dark-100 px-2 py-1 roun gap-x-1 mt-2">
+          </View>
+
+          <View className="flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2">
             <Image source={icons.star} className="size-4" />
+
             <Text className="text-white font-bold text-sm">
-              {Math.round(movie?.vote_average ?? 0)}
+              {Math.round(movie?.vote_average ?? 0)}/10
             </Text>
+
             <Text className="text-light-200 text-sm">
-              ({movie?.vote_count} votes)=
+              ({movie?.vote_count} votes)
             </Text>
           </View>
+
           <MovieInfo label="Overview" value={movie?.overview} />
           <MovieInfo
             label="Genres"
-            value={movie?.genres?.map((g) => g.name).join(" - ") || "N/A"}
+            value={movie?.genres?.map((g) => g.name).join(" • ") || "N/A"}
           />
+
           <View className="flex flex-row justify-between w-1/2">
             <MovieInfo
               label="Budget"
@@ -74,25 +87,27 @@ const MovieDetails = () => {
               )} million`}
             />
           </View>
+
           <MovieInfo
-            label="Productiın Companies"
+            label="Production Companies"
             value={
-              movie?.production_companies.map((c) => c.name).join(" - ") ||
+              movie?.production_companies?.map((c) => c.name).join(" • ") ||
               "N/A"
             }
           />
         </View>
       </ScrollView>
+
       <TouchableOpacity
-        className="absolute bottom-5 left-0 right-0 bg-accent rounded-lg py-3.5 flex flex-row items-center justify-center z-50"
+        className="absolute bottom-5 left-0 right-0 mx-5 bg-accent rounded-lg py-3.5 flex flex-row items-center justify-center z-50"
         onPress={router.back}
       >
         <Image
           source={icons.arrow}
           className="size-5 mr-1 mt-0.5 rotate-180"
-          tintColor={"#fff"}
+          tintColor="#fff"
         />
-        <Text className="text-white text-base font-semibold">Go back</Text>
+        <Text className="text-white font-semibold text-base">Go Back</Text>
       </TouchableOpacity>
     </View>
   );
